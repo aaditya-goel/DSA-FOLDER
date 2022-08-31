576a523fcc6dbc2b4558083302c98ff49bbcd97d
Aditya Goel <106088961+aaditya-goel@users.noreply.github.com>
2022-08-31 17:44:05 +0530

    Add files via upload

---
+++ SORTING/Selection_sorting.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Selection_sorting {
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 7, 8, 1, 3, 2 };
+
+        int l = arr.length;
+        // selection sort
+        for (int i = 0; i <= l - 2; i++) {
+            int smallest = i;
+            for (int j = i + 1; j <= l-1; j++) {
+                if (arr[j] < arr[smallest]) {
+                    smallest = j;
+                }
+            }
+            // swap
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+        printArray(arr);
+    }
+
+}
